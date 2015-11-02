0c5bf24c9476edded4c91ff4913bf3ff2a5863a2
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-11-02 22:27:42 +0000

    Code to read raw files, combine and create .RDA file written

--- RawDataAnalysis.R
+++ RawDataAnalysis.R
@@ -1,41 +1,42 @@
+processDat <- function(sourceDir="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset/1pct_samples")
+{
+  
 library(jsonlite)
-
-
-## Here just reading in the data and generating some smaller samples to take a look at it.
-
-setwd("/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset/1pct_samples")
-
+library(dplyr)
+setwd(sourceDir)
 set.seed(1974)
 
 ## The files are not valid JSON in their entirety. Each line of the file is a valid JSON doc.
 ## They can be read in using stream_in from the jsonlite package
-## Lets pull a sample of 1000 rows from each one.
 
-## FUll samples were hangning so used this one liner to create 1 percent sized samples
+## FUll samples were hanging so used this one liner to create 1 percent sized samples
 ## perl -ne 'print if (rand() < .01)' yelp_academic_dataset_user.json > 1pct_samples/user.json
 ## this came from the forums
 
 ## Business - this summarises the data about the business
 
-business <- stream_in(file('business.json'))
+business <- stream_in(file('yelp_academic_dataset_business.json'))
 
-## checkin tracks check-ins - this is where the customers check in at the business via the mobile app
-## some times the customers get a discount
-
-checkin <- stream_in(file('checkin.json'))
+business[c("hours", "open", "attributes", "type", "review_count", "neighborhoods", "stars")] <- list(NULL) # Remove unwanted columns
 
 ## Review contain customer reviews (funnily enough!)
 
-review <- stream_in(file('review.json'))
+review <- stream_in(file('yelp_academic_dataset_review.json'))
 
+review[c("type", "review_id", "votes")] <- list(NULL) # Remove unwanted columns
 
-## tips are ways for people to leave information about the business without posting a full review
+## Now join datasets together
 
-tip <- stream_in(file('tip.json'))
+joined <- inner_join(review, business)
 
-## User details
+## Now save data into a .rda file
 
-user <- stream_in(file('user.json'))
+fname <- paste(sourceDir, "yelpdata.rda", sep="/")
+
+save(joined, file=fname )
+
+}
+
 
 
 
