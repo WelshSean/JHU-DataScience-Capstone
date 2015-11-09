9f51b041451f7e8fb898d1fab689ddef09a95d0c
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-11-09 23:48:18 +0000

    checkpoint

--- RunTraining.R
+++ RunTraining.R
@@ -4,6 +4,7 @@
 library(rpart)
 library(rpart.plot)
 library(caret)
+library(randomForest)
 
 # Setup
 set.seed(1974)
@@ -27,7 +28,21 @@ if (file.exists(trainpath)){
   save(trainingData, file=trainpath)
 }
 
+# K-Fold Validation
+#numfolds=10
+#observed <- c()
+#predicted <- c()
+#folds <- createFolds(y=trainingData[[1]]$stars, k=numfolds, list = TRUE, returnTrain = TRUE)
+#for (i in 1:numfolds){
+#  foldtrain <- trainingData[[1]][folds[[i]],]
+#  foldtest <- trainingData[[1]][-folds[[i]],]
+  ##last <- ncol(foldtest)
+#  cartfit <- rpart(stars ~ . -business_id, data=foldtrain, method="class")
+#  observed <- c(observed, foldtest$stars)
+#  predicted <- c(predicted, predict(cartfit, foldtest, method="class"))
+#}
+
 
 cartModelFit <- rpart(stars ~ . -business_id, data = trainingData[[1]], method = "class")
-cartPredict <- predict(cartModelFit, newdata = trainingData[[1]], type = "class")
-cartCM <- confusionMatrix(cartPredict, trainingData[[1]]$stars)
+#cartPredict <- predict(cartModelFit, newdata = trainingData[[1]], type = "class")
+#cartCM <- confusionMatrix(cartPredict, trainingData[[1]]$stars)
