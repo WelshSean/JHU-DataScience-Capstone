4c4a0338d7c4806108a0e3a46225f3fd50049ded
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-11-09 22:06:19 +0000

    Basic training working

--- RunTraining.R
+++ RunTraining.R
@@ -3,6 +3,7 @@
 # Libraries
 library(rpart)
 library(rpart.plot)
+library(caret)
 
 # Setup
 set.seed(1974)
@@ -10,15 +11,23 @@ setwd("/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone")
 
 
 # Load Training Data
-#PATH="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset/1pct_samples"
-PATH="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset"
+PATH="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset/1pct_samples"
+#PATH="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset"
 load(paste(PATH, "yelptrain.rda", sep="/"))
 
-# Source other code
+trainpath <- paste(PATH, "trainingdata.rda", sep="/")
 
-source("./FeatureExtraction.R")
+if (file.exists(trainpath)){
+  print("Loading Training data from file") 
+  load(trainpath)
+} else {
+  print("Creating training data")
+  source("./FeatureExtraction.R")
+  trainingData <- extractFeatures(yelpreviews)
+  save(trainingData, file=trainpath)
+}
 
 
-# Processing!!!! :-)
-
-trainingData <- extractFeatures(yelpreviews)
+cartModelFit <- rpart(stars ~ . -business_id, data = trainingData[[1]], method = "class")
+cartPredict <- predict(cartModelFit, newdata = trainingData[[1]], type = "class")
+cartCM <- confusionMatrix(cartPredict, trainingData[[1]]$stars)
