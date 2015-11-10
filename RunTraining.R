3040cabd88a182ed9759f4c953518835e7f830c0
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-11-10 23:44:10 +0000

    Now also have Random Forest working

--- RunTraining.R
+++ RunTraining.R
@@ -45,12 +45,23 @@ if (file.exists(trainpath)){
 trn <- trainingData[[1]] 
 trn$number_stars <- as.numeric(as.character(trn$number_stars))
 
-
+### lm cant handle number_stars as factor
 lmFit <- lm(number_stars ~ . -business_id, data = trn)
 lmPredict <- predict(lmFit, newdata = trn)
 lmCM <- confusionMatrix(table(factor(lmPredict, levels=min(trn$number_stars):max(trn$number_stars)),factor(trn$number_stars, levels=min(trn$number_stars):max(trn$number_stars))))
 
+
+### cart can handle nstars as factor
 cartModelFit <- rpart(number_stars ~ . -business_id -number_stars, data = trn, method = "class")
 
 cartPredict <- predict(cartModelFit, newdata = trn, type = "class")
 cartCM <- confusionMatrix(cartPredict, trn$number_stars)
+
+# rf doesnt like formula notation here and doesnt like business_id being included!
+
+trn2 <- trn
+trn2$business_id <- NULL
+i <- match("number_stars", names(trn2))
+rfFit <- randomForest(trn2[-i], trn2[[i]])
+rfPredict <- predict(rfFit, newdata = trn2, type = "class")
+rfCM <- confusionMatrix(rfPredict, trn2$number_stars)
