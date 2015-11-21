07222e07c7e447d4e108fc51f9613b90fe4601b0
Sean Clarke <Sean@Seans-MacBook-Pro.local>
2015-11-21 11:01:32 +0000

    Added some slightly tweaked fits

--- RunTraining2.R
+++ RunTraining2.R
@@ -15,8 +15,8 @@ setwd("/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone")
 
 
 # Load Training Data
-PATH="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset/1pct_samples"
-#PATH="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset"
+#PATH="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset/1pct_samples"
+PATH="/Users/Sean/Coursera_DataScience/JHU-DataScience-Capstone/yelp_dataset_challenge_academic_dataset"
 load(paste(PATH, "yelptrain.rda", sep="/"))
 trainpath <- paste(PATH, "trainingdata.rda", sep="/")
 ctreepath <- paste(PATH, "ctreetraining.rda", sep="/")
@@ -25,6 +25,9 @@ RFpath <- paste(PATH, "RFtraining.rda", sep="/")
 SVMpath <- paste(PATH, "SVMtraining.rda", sep="/")
 trainpath <- paste(PATH, "trainingdata.rda", sep="/")
 lmpath <- paste(PATH, "lmtraining.rda", sep="/")
+NBpath <- paste(PATH, "NaiveBayestraining.rda", sep="/")
+lm2path <- paste(PATH, "lm2training.rda", sep="/")
+RF2path <- paste(PATH, "RF2training.rda", sep="/")
 
 if (file.exists(trainpath)){
   print("Loading Training data from file") 
@@ -99,6 +102,9 @@ if (!file.exists(lmpath))
 }
 
 
+
+# Random Forest default settings
+
 if (!file.exists(RFpath))
 {  
   print("Random Forest")
@@ -121,6 +127,141 @@ if (!file.exists(RFpath))
   RFRes$Times$End=date()
   
   save(RFRes, RFfit, file = RFpath)
+} else {
+  load(RFpath) 
+}
+
+## Ctree
+
+if (!file.exists(ctreepath))
+{  
+  print("ctree")
+  start=date()
+  #training <-   data.frame(lapply(trainingData, factor))
+  
+  ctreefit <- ctree(number_stars ~ pos_com+neg_com+review_length , data=training)
+  
+  
+  predicted <- predict(ctreefit, newdata=training[-number_stars_index])
+  
+  ctreeRes <- c()
+  
+  ctreeRes$Predictions <- as.data.frame(cbind(training$number_stars,predicted))
+  names(ctreeRes$Predictions) <- c("observed", "predicted")
+  ctreeRes$Predictions$Difference <- ctreeRes$Predictions$observed - ctreeRes$Predictions$predicted
+  ctreeRes$summary <- summary(ctreeRes$Predictions$Difference)
+  ctreeRes$RMSError <- sqrt(mean((ctreeRes$Predictions$Difference)^2))
+  ctreeRes$ExactMatch <- nrow(subset(ctreeRes$Predictions, ctreeRes$Predictions$observed==ctreeRes$Predictions$predicted))*100/nrow(ctreeRes$Predictions)
+  ctreeRes$Times$Start=start
+  ctreeRes$Times$End=date()
+  
+  save(ctreeRes, ctreefit, file = ctreepath)
+}
+
+# Naive Bayes
+
+if (!file.exists(NBpath))
+{  
+  print("Naive Bayes")
+  start=date()
+  training <-   data.frame(lapply(trainingData, factor))
+  
+  NBfit <- naiveBayes(number_stars ~ pos_com+neg_com+review_length , data=training)
+  
+  
+  predicted <- predict(NBfit, newdata=training[-number_stars_index], type="class")
+  NBRes <- c()
+  
+  NBRes$Predictions <- as.data.frame(cbind(training$number_stars,predicted))
+  names(NBRes$Predictions) <- c("observed", "predicted")
+  NBRes$Predictions$Difference <- NBRes$Predictions$observed - NBRes$Predictions$predicted
+  NBRes$summary <- summary(NBRes$Predictions$Difference)
+  NBRes$RMSError <- sqrt(mean((NBRes$Predictions$Difference)^2))
+  NBRes$ExactMatch <- nrow(subset(NBRes$Predictions, NBRes$Predictions$observed==NBRes$Predictions$predicted))*100/nrow(NBRes$Predictions)
+  NBRes$Times$Start=start
+  NBRes$Times$End=date()
+  
+  save(NBRes, NBfit, file = NBpath)
+}
+
+# # Support Vector Machine
+# 
+# if (!file.exists(SVMpath))
+# {
+#   print("SVM")
+#   start=date()
+#   SVMfit <- ksvm(number_stars ~ pos_com+neg_com+review_length,data=trainingData,kernel="rbfdot",kpar=list(sigma=0.05),C=5,cross=3, verbose=TRUE)
+#   
+#   predicted <- predict(SVMfit, newdata=trainingData[-c(number_stars_index)])
+#   
+#   SVMRes <- c()
+#   
+#   SVMRes$Predictions <- as.data.frame(cbind(trainingData$number_stars,predicted))
+#   names(SVMRes$Predictions) <- c("observed", "predicted")
+#   SVMRes$Predictions$Difference <- SVMRes$Predictions$observed - SVMRes$Predictions$predicted
+#   SVMRes$summary <- summary(SVMRes$Predictions$Difference)
+#   SVMRes$RMSError <- sqrt(mean((SVMRes$Predictions$Difference)^2))
+#   SVMRes$ExactMatch <- nrow(subset(SVMRes$Predictions, SVMRes$Predictions$observed==SVMRes$Predictions$predicted))*100/nrow(SVMRes$Predictions)
+#   SVMRes$Times$Start=start
+#   SVMRes$Times$End=date()
+#   
+#   save(SVMRes, SVMfit, file = SVMpath)
+# }
+
+# Linear Model #2
+
+if (!file.exists(lm2path))
+{
+  print("lm2")
+  start=date()
+  lm2training <- trainingData
+  lm2training$number_stars <- as.integer(as.character(lm2training$number_stars))
+  
+  lm2fit <- lm(number_stars ~ . -business_id, data = lm2training)
+  
+  
+  predicted <- predict(lm2fit, newdata=lm2training[-c(number_stars_index)])
+  
+  lm2Res <- c()
+  
+  lm2Res$Predictions <- as.data.frame(cbind(lm2training$number_stars,predicted))
+  names(lm2Res$Predictions) <- c("observed", "predicted")
+  lm2Res$Predictions$observed <- as.integer(lm2Res$Predictions$observed)
+  lm2Res$Predictions$predicted <- as.integer(lm2Res$Predictions$predicted)
+  lm2Res$Predictions$Difference <- lm2Res$Predictions$observed - lm2Res$Predictions$predicted
+  lm2Res$summary <- summary(lm2Res$Predictions$Difference)
+  lm2Res$RMSError <- sqrt(mean((lm2Res$Predictions$Difference)^2))
+  lm2Res$ExactMatch <- nrow(subset(lm2Res$Predictions, lm2Res$Predictions$observed==lm2Res$Predictions$predicted))*100/nrow(lm2Res$Predictions)
+  lm2Res$Times$Start=start
+  lm2Res$Times$End=date()
+  
+  save(lm2Res, lm2fit, file = lm2path)
+}
+
+# Random Forest ntree =1000
+
+if (!file.exists(RF2path))
+{  
+  print("Random Forest 2")
+  start=date()
+  
+  RF2fit <- randomForest(number_stars ~ . -business_id, data=trainingData, ntree=1000)
+  
+  
+  predicted <- predict(RF2fit, newdata=trainingData[-number_stars_index], type="class")
+  
+  RF2Res <- c()
+  
+  RF2Res$Predictions <- as.data.frame(cbind(trainingData$number_stars,predicted))
+  names(RF2Res$Predictions) <- c("observed", "predicted")
+  RF2Res$Predictions$Difference <- RF2Res$Predictions$observed - RF2Res$Predictions$predicted
+  RF2Res$summary <- summary(RF2Res$Predictions$Difference)
+  RF2Res$RMSError <- sqrt(mean((RF2Res$Predictions$Difference)^2))
+  RF2Res$ExactMatch <- nrow(subset(RF2Res$Predictions, RF2Res$Predictions$observed==RF2Res$Predictions$predicted))*100/nrow(RF2Res$Predictions)
+  RF2Res$Times$Start=start
+  RF2Res$Times$End=date()
+  
+  save(RF2Res, RF2fit, file = RF2path)
 } else {
   load(RFpath) 
 }
